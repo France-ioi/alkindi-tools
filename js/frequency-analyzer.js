f1f4a93e09d7c50945e8ab4814e9a236fb568275
Guillaume Duhamel <guillaume.duhamel@edukera.com>
2015-10-29 17:59:09 +0100

    fix ie

--- js/frequency-analyzer.js
+++ js/frequency-analyzer.js
@@ -1,3 +1,26 @@
+if (!Array.prototype.indexOf)
+{
+  Array.prototype.indexOf = function(elt /*, from*/)
+  {
+    var len = this.length >>> 0;
+
+    var from = Number(arguments[1]) || 0;
+    from = (from < 0)
+         ? Math.ceil(from)
+         : Math.floor(from);
+    if (from < 0)
+      from += len;
+
+    for (; from < len; from++)
+    {
+      if (from in this &&
+          this[from] === elt)
+        return from;
+    }
+    return -1;
+  };
+}
+
 function MyMap() {
     this.keys = [];
     this.values = [];
@@ -344,7 +367,7 @@ function frequency_analyzer(iTextInput, iFont) {
 
 	var lTokens = new Array();
 	for (var i = 0, len = iText.length; i < len; i++) {
-	    var lRef = iText[i];
+	    var lRef = iText.charAt(i);
 	    var lValue = lRef;
 	    var lMute = false;
 	    var lWithAccent = false;
